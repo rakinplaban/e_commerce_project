22c7a409c2aed70249fb76fe35e7e3c4da963cc3
rakinplaban <rakinshahriar54@gmail.com>
2021-04-29 13:16:46 +0600

    make migration

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class auction_listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    category = models.CharField(max_length=120)
+    img = models.URLField()
+
+    def __str__(self):
+        return f"Title {title} : Category {category} Starting price {starting_bid}"
