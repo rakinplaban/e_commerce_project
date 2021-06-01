47a413d87a63f1240a84ba42d2cf07f98c198c4b
rakinplaban <rakinshahriar54@gmail.com>
2021-06-01 11:43:18 +0600

    display auction listing in a list view in admin page.

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from .models import User,auction_listing
 # Register your models here.
 
+class auction_listing_admin(admin.ModelAdmin):
+    list_display = ("id","title","category","starting_bid")
+
+
 admin.site.register(User,)
-admin.site.register(auction_listing,)
+admin.site.register(auction_listing,auction_listing_admin)
