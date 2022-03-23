2d67a9c3e26960de8f7bb2a23df62ae1ed4bbb7d
rakinplaban <rakinshahriar54@gmail.com>
2022-03-23 20:22:13 +0600

    added all

--- commerce/settings.py
+++ commerce/settings.py
@@ -140,4 +140,6 @@ MESSAGE_TAGS = {
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 django_heroku.settings(locals())
